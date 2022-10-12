e59edc0c60006e6fd4566e63400bed455dc7f8c3
Andrew <andrew.n.maclean@gmail.com>
2022-10-11 17:35:40 -0700

    updated logger

--- backyard-be/Config/logger.js
+++ backyard-be/Config/logger.js
@@ -1,5 +1,8 @@
 const Logger = require('byf-custom-logger')
 
-const logger = new Logger({ name: 'friends', verbose: (process.env.NODE_ENV === 'dev') })
-
+const logger = new Logger({
+  name: 'friends',
+  verbose: process.env.NODE_ENV === 'dev',
+  storeLogs: false
+})
 module.exports = logger
