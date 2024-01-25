67001638256d96d8bda8eecec0adfeb6f27dd3e7
Andrew Maclean <andrew.n.maclean@gmail.com>
2024-01-24 23:18:45 -0800

    certbot port

--- avys/index.js
+++ avys/index.js
@@ -1,4 +1,5 @@
 const spdy = require('spdy')
+const http = require('http')
 const fs = require('fs')
 const path = require('path')
 const WebSocket = require('ws')
@@ -7,6 +8,7 @@ const app = require('@amaclean2/sundaypeak-rivers')
 const { onConnection } = require('@amaclean2/sundaypeak-couloirs')
 
 const PORT = process.env.PORT || 5000
+const HTTP_PORT = process.env.HTTP_PORT || 80
 
 // created by following https://www.youtube.com/watch?v=USrMdBF0zcg
 
@@ -33,3 +35,21 @@ wss.on('connection', onConnection)
 server.listen(PORT, () =>
   console.log(`Sunday Peak API/Websocket listening on port ${PORT}`)
 )
+
+// certbot server
+http
+  .createServer((req, res) => {
+    if (req.url.includes('/.well-known/acme-challenge')) {
+      // list all the files in the html directory
+      const files = fs.readdirSync('./var/www/html')
+      // read the first file
+      const certbotFiles = fs.readFileSync('./var/www/html', files[0])
+      // output the contents from the first file
+      res.write(certbotFiles)
+      res.end()
+    } else {
+      res.write('this path is not designed for this')
+      res.end()
+    }
+  })
+  .listen(HTTP_PORT)
